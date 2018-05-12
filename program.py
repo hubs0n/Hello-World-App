5cf94e56ee3f22ffc2e890199099e1cae67a9f5c
Hubert Czajka <hubertczajka@MacBook-Pro-Hubert.local>
2018-05-12 23:34:30 +0200

    Initial commit

---
+++ program.py
@@ -0,0 +1,9 @@
+print('----------------------------')
+print('        Hello World!')
+print('----------------------------')
+print()
+
+user_text = input('What is your name? ')
+greetings = "Nice to meet you " + user_text
+
+print(greetings)
